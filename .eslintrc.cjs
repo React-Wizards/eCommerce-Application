7d004bfe0b5f85753ec8a9a62306a20ba690da60
C0lgate <k0lesan2k00@gmail.com>
2024-05-07 15:52:14 +0300

    core/RSS-ECOMM-2_24: set-up-project (#19)
    * core: install tailwindcss
    
    * fix: correct alias
    
    * core: add env variables
    
    * core: install sass

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,12 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: [
+    'dist',
+    '.eslintrc.cjs',
+    'tailwind.config.js',
+    'postcss.config.js'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: ['./tsconfig.json', './tsconfig.node.json']

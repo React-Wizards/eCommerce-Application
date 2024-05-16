0b2f47e07c5fa9144f5a0c6507856f1ae9a4abb0
z-e-a <e.a.zubkov@yandex.ru>
2024-05-17 00:55:31 +0300

    feat: add field validators

---
+++ src/pages/registration-page/lib/validators/only-letters.ts
@@ -0,0 +1,12 @@
+import { Validator } from '../../model/types';
+
+const REGEX = /[а-яА-Яa-zA-Z]+/gi;
+
+const ERROR_MESSAGE = 'Must contain only letters';
+
+const onlyLetters: () => Validator<string> = (errorMessage = ERROR_MESSAGE) => {
+  return async (value) =>
+    value.replace(REGEX, '').length == 0 ? null : `${errorMessage}`;
+};
+
+export default onlyLetters;

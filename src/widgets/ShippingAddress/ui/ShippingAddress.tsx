6965d76e1fe8f9b9937f64a1b65c52209c0191f4
z-e-a <e.a.zubkov@yandex.ru>
2024-05-17 01:03:44 +0300

    feat: update widgets and common elements

--- src/widgets/ShippingAddress/ui/ShippingAddress.tsx
+++ src/widgets/ShippingAddress/ui/ShippingAddress.tsx
@@ -1,8 +1,21 @@
 import Address from '@/features/Address';
 import BlockDivider from '@/shared/BlockDivider';
 import styles from './ShippingAddress.module.scss';
+import { ValidableField } from '@/pages/registration-page/model/types';
 
-const ShippingAddress = () => {
+const ShippingAddress: React.FC<{
+  fieldsList: Array<ValidableField>;
+  isSameAddress: boolean;
+  onCheckSameAddressHandler: () => void;
+  isShippingDefault: boolean;
+  onCheckShippingDefaultHandler: () => void;
+}> = (props: {
+  fieldsList: Array<ValidableField>;
+  isSameAddress: boolean;
+  onCheckSameAddressHandler: () => void;
+  isShippingDefault: boolean;
+  onCheckShippingDefaultHandler: () => void;
+}) => {
   return (
     <>
       <BlockDivider text={'Shipping address'} />
@@ -11,16 +24,20 @@ const ShippingAddress = () => {
           className={styles['form-checkbox-left']}
           type='checkbox'
           id='same-as-bill'
+          checked={props.isSameAddress}
+          onChange={props.onCheckSameAddressHandler}
         />
         Same as billing address
       </label>
-      <Address />
+      <Address fieldsList={props.fieldsList} isDisabled={props.isSameAddress} />
       <label className={styles['from-text__right']} htmlFor='shipp-default'>
         Set as default shipping address
         <input
           className={styles['form-checkbox-right']}
           type='checkbox'
           id='shipp-default'
+          checked={props.isShippingDefault}
+          onChange={props.onCheckShippingDefaultHandler}
         />
       </label>
     </>

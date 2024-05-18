62d464a34e13dc02fd6a31ab3f49227f42ff367a
K0LESAN <k0lesan2k00@gmail.com>
2024-05-18 20:35:11 +0300

    feat: create redirect to NotFound page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from '@/pages/loginPage';
 import RegistrationPage from '@/pages/registration-page';
+import NotFound from '@/pages/NotFound';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegistrationPage />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );

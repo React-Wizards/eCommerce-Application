e92525f9a054ac85e2dc29b5497a067c2f0d6190
K0LESAN <k0lesan2k00@gmail.com>
2024-05-23 04:14:53 +0300

    feat: install depedencies for typescript sdk

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,37 @@
-import { defineConfig } from 'vite';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 import React from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [React()],
   base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          process: true,
+          buffer: true
+        }),
+        NodeModulesPolyfillPlugin()
+      ]
+    }
+  },
+  build: {
+    rollupOptions: {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      plugins: [rollupNodePolyFill()]
     }
   }
 });

6489b81accb37802fd1da43c72bfcccab64b2f42
K0LESAN <k0lesan2k00@gmail.com>
2024-05-24 05:30:29 +0300

    refactor: replace router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,22 +1,8 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from '@/pages/loginPage';
-import RegistrationPage from '@/pages/registration-page';
-import NotFound from '@/pages/NotFound';
-import Home from '@/pages/Home';
+import Router from './Router';
 import './App.scss';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Navigate to='/home' replace />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegistrationPage />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const App = () => {
+  return <Router />;
+};
 
 export default App;

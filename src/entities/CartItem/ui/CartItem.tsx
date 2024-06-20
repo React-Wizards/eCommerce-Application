92a3db56a14c900d7e146d1575f16913e06a7141
K0LESAN <k0lesan2k00@gmail.com>
2024-06-20 12:12:26 +0300

    feat: add cart item

--- src/entities/CartItem/ui/CartItem.tsx
+++ src/entities/CartItem/ui/CartItem.tsx
@@ -1,79 +1,178 @@
-import { type ChangeEvent, useState } from 'react';
+import { type ChangeEvent, useEffect, useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import type { RootState } from '@/app/store';
+import type { Cart, LineItem } from '@commercetools/platform-sdk';
+import imageNotFound from '@/shared/assets/img/cart-image-not-found.jpg';
+import {
+  useAddProductToCartMutation,
+  useDeleteProductFromCartMutation,
+  useRecalculateMutation
+} from '@/features/api/meApi';
+import { setCart } from '@/entities/cart';
+import { useDispatch } from 'react-redux';
+import Loader from '@/shared/Loader';
+import { useDebounce } from '@/shared/useDebounce';
+import { FaRegTrashCan } from 'react-icons/fa6';
 import styles from './CartItem.module.scss';
+import { formatPriceString } from '@/shared/utils';
 
-interface IBasket {
-  name: string;
-  price: number;
-  image: string;
-  size: string;
-  quantity: number;
+interface IProps {
+  lineItem: LineItem;
 }
 
-const CartItem = ({ basket }: { basket: IBasket }) => {
-  const [quantity, setQuantity] = useState<number>(basket.quantity);
-  const { name, price, image, size }: IBasket = basket;
+interface IRequestInfo {
+  cartId: string;
+  cartVersion: number;
+}
+
+const CartItem = ({ lineItem }: IProps) => {
+  const [quantity, setquantity] = useState<number>(lineItem.quantity);
+  const debouncedQuantity: number = useDebounce<number>(quantity, 400);
+  const [addProductToCart, { isLoading: addProductIsLoading }] =
+    useAddProductToCartMutation();
+  const [deleteProductFromCart, { isLoading: deleteProductIsLoading }] =
+    useDeleteProductFromCartMutation();
+  const [recalculate, { isLoading: recalculateIsLoading }] =
+    useRecalculateMutation();
+  const urlToImage: string = lineItem.variant.images?.[0].url || imageNotFound;
+  const name: string = lineItem.name['en-US'];
+  const cart: Cart = useSelector<RootState, Cart>(
+    (store: RootState): Cart => store.cart.cart!
+  );
+  const dispatch = useDispatch();
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    if (firstRender.current) {
+      firstRender.current = false;
+      return;
+    }
+
+    if (debouncedQuantity === lineItem.quantity) {
+      return;
+    }
+
+    const fetchCart = async (): Promise<void> => {
+      const requestInfo: IRequestInfo = {
+        cartId: cart.id,
+        cartVersion: cart.version
+      };
+      const newQuantity: number = Math.abs(
+        debouncedQuantity - lineItem.quantity
+      );
+
+      try {
+        const newCart: Cart = await (
+          debouncedQuantity > lineItem.quantity
+            ? addProductToCart({
+                ...requestInfo,
+                productId: lineItem.productId,
+                quantity: newQuantity
+              })
+            : deleteProductFromCart({
+                ...requestInfo,
+                lineItemId: lineItem.id,
+                lineItemQuantity: newQuantity
+              })
+        ).unwrap();
+
+        dispatch(
+          setCart(
+            await recalculate({
+              cartId: newCart.id,
+              cartVersion: newCart.version
+            }).unwrap()
+          )
+        );
+      } catch (error: unknown) {
+        console.error(error);
+      }
+    };
+
+    fetchCart();
+  }, [debouncedQuantity]);
 
   return (
-    <>
-      <tr>
-        <td>
-          <div className={styles.wrapper}>
-            <img src={image} alt={name} />
-            <div className={styles.info}>
-              <span title={name}>{name}</span>
-              <span>Size: {size}</span>
-            </div>
+    <tr className={styles.row}>
+      <td className={styles.cell}>
+        {(addProductIsLoading ||
+          deleteProductIsLoading ||
+          recalculateIsLoading) && <Loader />}
+        <div className={styles.wrapper}>
+          <img className={styles.image} src={urlToImage} alt={name} />
+          <div className={styles.info}>
+            <span className={styles.info__text} title={name}>
+              {name}
+            </span>
           </div>
-        </td>
-        <td>${price.toFixed(2)}</td>
-        <td>
-          <button
-            className={styles.button}
-            onClick={() => {
-              setQuantity((prev: number): number => Math.max(0, prev - 1));
-            }}>
-            -
-          </button>
-          <input
-            className={styles.quantity}
-            type='number'
-            min='1'
-            max='999'
-            minLength={1}
-            value={quantity}
-            pattern='[1-9][0-9]*'
-            style={{
-              textAlign: 'center',
-              width: '50px',
-              marginInline: '5px',
-              border: '2px solid black'
-            }}
-            onChange={(event: ChangeEvent<HTMLInputElement>): void => {
-              const currentQuantity: number = Number(event.target.value);
-              const min: number = Number(event.target.min);
-              const max: number = Number(event.target.max);
+        </div>
+      </td>
+      <td className={styles.cell}>
+        <div className={styles.cell__wrapper}>
+          <span
+            className={`${styles.cell__price} ${lineItem.price.discounted ? styles.cell__price_discounted : ''}`}>
+            {formatPriceString(lineItem.price.value)}
+          </span>
+          {lineItem.price.discounted && (
+            <span className={`${styles.cell__price}`}>
+              {formatPriceString(lineItem.price.discounted.value)}
+            </span>
+          )}
+        </div>
+      </td>
+      <td className={`${styles.cell} ${styles.cell__quantity}`}>
+        <button
+          className={styles.button}
+          onClick={() => {
+            setquantity((prev: number): number => Math.max(0, prev - 1));
+          }}>
+          <span className={styles.button__text}>-</span>
+        </button>
+        <input
+          className={styles.quantity}
+          type='number'
+          min='1'
+          max='999'
+          minLength={1}
+          value={quantity}
+          pattern='[1-9][0-9]*'
+          onChange={(event: ChangeEvent<HTMLInputElement>): void => {
+            setquantity(Number(event.target.value));
+          }}
+        />
+        <button
+          className={styles.button}
+          onClick={() => {
+            setquantity((prev: number): number => prev + 1);
+          }}>
+          <span className={styles.button__text}>+</span>
+        </button>
+      </td>
+      <td className={`${styles.cell} ${styles.cell__total}`}>
+        {formatPriceString(lineItem.totalPrice)}
+      </td>
+      <td className={styles.cell}>
+        <button
+          className={styles.delete}
+          onClick={() => {
+            const clearCart = async (): Promise<void> => {
+              dispatch(
+                setCart(
+                  await deleteProductFromCart({
+                    cartId: cart.id,
+                    cartVersion: cart.version,
+                    lineItemId: lineItem.id
+                  }).unwrap()
+                )
+              );
+            };
 
-              if (currentQuantity < min || currentQuantity > max) {
-                return;
-              }
-
-              setQuantity(currentQuantity);
-            }}
-          />
-          <button
-            className={styles.button}
-            onClick={() => {
-              setQuantity((prev: number): number => prev + 1);
-            }}>
-            +
-          </button>
-        </td>
-        <td>${(price * quantity).toFixed(2)}</td>
-        <td>
-          <button>delete</button>
-        </td>
-      </tr>
-    </>
+            clearCart();
+          }}>
+          <FaRegTrashCan className={styles.delete__icon} size={22} />
+        </button>
+      </td>
+    </tr>
   );
 };
 

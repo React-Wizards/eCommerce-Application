8dcedc94bdb2fba1d35fb220a48b31f840a215bf
K0LESAN <k0lesan2k00@gmail.com>
2024-06-20 12:16:28 +0300

    feat: add empty cart message component

---
+++ src/widgets/EmptyCartMessage/ui/EmptyCartMessage.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from 'react-router-dom';
+import emptyCart from '@/shared/assets/img/empty-cart.png';
+import styles from './EmptyCartMessage.module.scss';
+
+const EmptyCartMessage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.wrapper}>
+      <img className={styles.image} src={emptyCart} alt='Empty Cart' />
+      <p className={styles.title}>Your cart is empty!</p>
+      <button
+        className={styles.button}
+        onClick={() => {
+          navigate('/home');
+        }}>
+        Go to shop
+      </button>
+    </div>
+  );
+};
+
+export default EmptyCartMessage;
